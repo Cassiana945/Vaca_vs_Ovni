b1655158dddf24ac6b2854e5ca894d02bac3368d
Cassiana <160614428+Cassiana945@users.noreply.github.com>
2025-06-01 14:15:23 -0300

    Update MainActivity.java

--- java/com/example/vacavsovni/activities/MainActivity.java
+++ java/com/example/vacavsovni/activities/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +15,6 @@ import com.example.vacavsovni.R;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton botaoJogar;
-    private TextView tituloJogo;
     private MediaPlayer player;
 
     @Override
@@ -26,16 +24,12 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         botaoJogar = findViewById(R.id.botaoJogar);
-        tituloJogo = findViewById(R.id.tituloJogo);
         player = MediaPlayer.create(MainActivity.this, R.raw.som_clicar_botao);
-
-        Typeface fonte = Typeface.createFromAsset(getAssets(), "font/kenney_blocks.ttf");
-        tituloJogo.setTypeface(fonte);
 
         botaoJogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GameView.class);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
                 player.start();
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
